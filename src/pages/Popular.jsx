386c4add1327ad1e165b93a5c3dcb2eba02eb802
Sergey-Okey <sergeyborisov_1@vk.ru>
2024-04-23 14:51:18 +0300

    update

--- src/pages/Popular.jsx
+++ src/pages/Popular.jsx
@@ -1,86 +1,37 @@
-import '../assets/css/popular.css'
-import { useEffect, useState } from 'react'
-import { fetchRandomPosts } from '../components/api'
+import '../assets/css/popular.css';
+import { useEffect, useState } from 'react';
+import { fetchRandomPosts } from '../components/api';
 
 export default function Popular() {
-	const [randomPost, setRandomPost] = useState(null)
+	const [randomPosts, setRandomPosts] = useState([]);
 
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
-				const post = await fetchRandomPosts()
-				setRandomPost(post)
+				const posts = [];
+				// Получаем 10 случайных постов
+				for (let i = 0; i < 20; i++) {
+					const post = await fetchRandomPosts();
+					posts.push(post);
+				}
+				setRandomPosts(posts);
 			} catch (error) {
-				console.error('Ошибка при получении случайного поста:', error)
+				console.error('Ошибка при получении случайных постов:', error);
 			}
 		};
 
-		fetchData()
+		fetchData();
 	}, []);
 
 	return (
 		<div className="container">
 			<div className="popular">
-				{randomPost && (
-					<div className='post-item'>
-						<h2>{randomPost.title}</h2>
-						<p>{randomPost.body}</p>
+				{randomPosts.map((post, index) => (
+					<div key={index} className='post-item'>
+						<h2>{post.title}</h2>
+						<p>{post.body}</p>
 					</div>
-				)}
-				{randomPost && (
-					<div className='post-item'>
-						<h2>{randomPost.title}</h2>
-						<p>{randomPost.body}</p>
-					</div>
-				)}
-				{randomPost && (
-					<div className='post-item'>
-						<h2>{randomPost.title}</h2>
-						<p>{randomPost.body}</p>
-					</div>
-				)}
-				{randomPost && (
-					<div className='post-item'>
-						<h2>{randomPost.title}</h2>
-						<p>{randomPost.body}</p>
-					</div>
-				)}
-				{randomPost && (
-					<div className='post-item'>
-						<h2>{randomPost.title}</h2>
-						<p>{randomPost.body}</p>
-					</div>
-				)}
-				{randomPost && (
-					<div className='post-item'>
-						<h2>{randomPost.title}</h2>
-						<p>{randomPost.body}</p>
-					</div>
-				)}
-				{randomPost && (
-					<div className='post-item'>
-						<h2>{randomPost.title}</h2>
-						<p>{randomPost.body}</p>
-					</div>
-				)}
-				{randomPost && (
-					<div className='post-item'>
-						<h2>{randomPost.title}</h2>
-						<p>{randomPost.body}</p>
-					</div>
-				)}
-				{randomPost && (
-					<div className='post-item'>
-						<h2>{randomPost.title}</h2>
-						<p>{randomPost.body}</p>
-					</div>
-				)}
-				{randomPost && (
-					<div className='post-item'>
-						<h2>{randomPost.title}</h2>
-						<p>{randomPost.body}</p>
-					</div>
-				)}
+				))}
 			</div>
 		</div>
 	);
